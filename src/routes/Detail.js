85a0b65f175fc0555439a0687954e88e70a7b68e
unknown <jjj1305@hanmail.net>
2021-07-29 20:29:05 +0900

    lecture 4.1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,21 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+// import { useParams } from "react-router-dom";
+
+function Detail({ toDo }){
+    // const id = useParams(); // React Hooks 라고 함, useParams() 대신에 mapStateToProps의 state에서 id를 가져와서 이용해보도록 하자
+    return (
+        <>
+            <h1>{toDo?.text}</h1>
+            <h5>Create at: {toDo?.id}</h5>
+        </>
+    )
+}
+
+function mapStateToProps(state, ownProps){
+    // console.log(ownProps);              // console을 보면, props 중에 match가 있고 match 안에는 params:{id:"~~~"} 가 있다.
+    const {match: {params: {id}}} = ownProps;   // 위에서 ownProps를 통해 얻은 id를 활용해보자.
+    return { toDo: state.find(toDo => toDo.id === parseInt(id)) };   // find는 testing function에 만족하는 첫번째 요소를 반환(첫번째 요소만 반환하는 것이 filter와는 다르다)
+}   
+
+export default connect(mapStateToProps)(Detail);
--- src/routes/store.js
+++ src/routes/store.js
@@ -1,28 +1,41 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
+
 
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-const addToDo = text => {
-    return {
-        type: ADD,
-        text
-    };
-};
 
-const deleteToDo = id => {
-    return {
-        type: DELETE,
-        id
-    };
-};
 
-const reducer = (state = [], action) => {
-    switch (action.type) {
-        case ADD:
+// const addToDo = text => {
+//     return {
+//         type: ADD,
+//         text
+//     };
+// };
+
+const addToDo = createAction("ADD");
+
+// const deleteToDo = id => {
+//     return {
+//         type: DELETE,
+//         id
+//     };
+// };
+
+const deleteToDo = createAction("DELETE");
+
+console.log(addToDo(), deleteToDo());       // 이렇게 함수를 호출해보면, type과 payload과 함께 나옴을 볼 수 있다.
+                                            // 또한 Add를 해도 To do 가 빈상태로 나타나는데, 이는 Home.js에서 addToDo가 text를 받는 것으로 했는데, toolkit에서는 payload로 바뀐다. 자동으로. 일종의 관행(convention)임
+                                            // toolkit에서는 action에 보내고 싶어하는 정보가 무엇이던지 payload에 담겨서 보내진다.
+
+const reducer = (state = [], action) =>{
+    switch (action.type){
+        case addToDo.type:
+            console.log(action);            // 위에서 설명한 것을 확인해보자. console을 보면 payload에 To do 로 Add 해준 것이 들어가있음
             return [{ text: action.text, id: Date.now() }, ...state];
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
+        case deleteToDo.type:
+            return state.filter(toDo => toDo.id !== action.payload); // toolkit의 createAction으로 deleteToDo를 만들었으므로 action은 더 이상 id를 갖지 않는다. payload로 대체해야 DEL 버튼이 작동해서 삭제가 된다.
         default:
             return state;
     }
