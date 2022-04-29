90de07940438f0c76aa6eb0cff5720aed9fdad15
Taltal <taltalrealty@gmail.com>
2022-04-29 19:21:16 +0900

    .

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./components/App";
 import store from "./routes/store";
 
 ReactDOM.render(
-    <Provider store={store}>      
+    <Provider store={store}>          
         <App />
     </Provider>,    // src/routes/store.js의 store와 연결시켜주기 위해 react-redux의 Provider 사용
     document.getElementById("root")
